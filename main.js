774fc8d15de4e770d3a79a68c41c74c496a439c9
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2022-04-14 12:26:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-function preload(){}
+nose_x = 0;
+nose_y = 0;
+
+function preload()
+{
+    lips_image = loadImage("lips.png");
+}
 
 function setup()
 {
-    canvas = createCanvas(300, 300);
+    canvas = createCanvas(350, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300, 300);
+    video.size(350, 300);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -23,13 +29,16 @@ function gotPoses(results)
     {
         console.log(results);
         console.log("nose x =" + results[0].pose.nose.x);
-        console.log("noes y =" + results[0].pose.nose.y)
+        console.log("noes y =" + results[0].pose.nose.y);
+        nose_x = results[0].pose.nose.x-20;
+        nose_y = results[0].pose.nose.y+10;
     }
 }
 
 function draw()
 {
-    image(video, 0, 0, 300, 300);
+    image(video, 0, 0, 350, 300);
+    image(lips_image, nose_x, nose_y, 40, 30);
 }
 
 function take_Snapshot()
